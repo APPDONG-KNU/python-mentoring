6c9f1fc4d8090f0c875aa26aef80f0d5b79a46d3
Jw Park <128016602+car3672@users.noreply.github.com>
2023-03-29 20:40:55 +0900

    Create 9498_Jungwoo.py

---
+++ 2023-03-29/9498_Jungwoo.py
@@ -0,0 +1,11 @@
+a = int(input())
+if (90<=a)and(a<=100):
+     print('A')
+if (80<=a)and(a<90):
+    print('B')
+if (70<=a)and(a<80):
+    print('C')
+if (60<=a)and(a<70):
+    print ('D')
+if (0<=a)and(a<60):
+     print('F')
