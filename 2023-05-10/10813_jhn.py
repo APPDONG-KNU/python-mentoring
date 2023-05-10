f7c1f38b497b6d0de81d0697ae500b48d793441b
Wjsgusska <128573822+Wjsgusska@users.noreply.github.com>
2023-05-10 21:18:26 +0900

    Create 10813_jhn.py

---
+++ 2023-05-10/10813_jhn.py
@@ -0,0 +1,10 @@
+baskets = []
+num, changes = map(int, input().split())
+for n in range(num+1):
+    baskets.append(n)
+for i in range(changes):
+    b1,b2 = map(int,input().split())
+    baskets[b1],baskets[b2] = baskets[b2],baskets[b1]
+baskets.pop(0)
+for b in baskets:
+    print(b, end=" ")
